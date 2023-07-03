f0a61e4c1b18e74ae45f2ab373e47468a6e1ac3a
onyenze <chibuezeonyeze123@gmail.com>
2023-07-04 00:07:47 +0100

    First Python Push

--- test.py
+++ test.py
@@ -2,3 +2,9 @@
 # I am taking my python journey seriously from now on
 # See you at the top in no time
 # Six months from now will be a very different Chibueze
+
+
+# Tuesday 4th of July, I am yet to do anything with Python and I have a project at hand
+# I have just been sooo fucking lazy and lacking the right motivation
+# Need very serious help at the moment
+# But i plan to change all that very soon, I strongly believe in myself and all of my dreams
